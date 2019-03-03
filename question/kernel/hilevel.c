7c0c589a47a477683c9ba0a931666e762bbcc195
kartsridhar <contactkarthik34@gmail.com>
2019-03-03 12:19:29 +0000

    stage 1a done

--- question/kernel/hilevel.c
+++ question/kernel/hilevel.c
@@ -7,14 +7,153 @@
 
 #include "hilevel.h"
 
-void hilevel_handler_rst() {
+//------------------------FROM lab-3_q-------------------------------------
+
+pcb_t pcb[ 2 ]; 
+pcb_t* current = NULL;
+
+void dispatch( ctx_t* ctx, pcb_t* prev, pcb_t* next ) {
+  char prev_pid = '?', next_pid = '?';
+
+  if( NULL != prev ) {
+    memcpy( &prev->ctx, ctx, sizeof( ctx_t ) ); // preserve execution context of P_{prev}
+    prev_pid = '0' + prev->pid;
+  }
+  if( NULL != next ) {
+    memcpy( ctx, &next->ctx, sizeof( ctx_t ) ); // restore  execution context of P_{next}
+    next_pid = '0' + next->pid;
+  }
+
+    PL011_putc( UART0, '[',      true );
+    PL011_putc( UART0, prev_pid, true );
+    PL011_putc( UART0, '-',      true );
+    PL011_putc( UART0, '>',      true );
+    PL011_putc( UART0, next_pid, true );
+    PL011_putc( UART0, ']',      true );
+
+    current = next;                             // update   executing index   to P_{next}
+
   return;
 }
 
-void hilevel_handler_irq() {
+void schedule( ctx_t* ctx ) {
+  if     ( current->pid == pcb[ 0 ].pid ) {
+    dispatch( ctx, &pcb[ 0 ], &pcb[ 1 ] );      // context switch P_3 -> P_4
+
+    pcb[ 0 ].status = STATUS_READY;             // update   execution status  of P_3
+    pcb[ 1 ].status = STATUS_EXECUTING;         // update   execution status  of P_4
+  }
+  else if( current->pid == pcb[ 1 ].pid ) {
+    dispatch( ctx, &pcb[ 1 ], &pcb[ 0 ] );      // context switch P_4 -> P_3
+
+    pcb[ 1 ].status = STATUS_READY;             // update   execution status  of P_4
+    pcb[ 0 ].status = STATUS_EXECUTING;         // update   execution status  of P_3
+  }
+
   return;
 }
 
-void hilevel_handler_svc() {
-  return;
+extern void     main_P3();
+extern uint32_t tos_P3;
+extern void     main_P4();
+extern uint32_t tos_P4;
+
+//-------------------------------------------------------------------------------
+
+void hilevel_handler_rst( ctx_t* ctx ) {
+      
+//       PL011_putc( UART0, 'X', true);
+    
+      // Initialise 2 PCBs = User Processes (from lab-3_q)
+      memset( &pcb[ 0 ], 0, sizeof( pcb_t ) );     // initialise 0-th PCB = P_3
+      pcb[ 0 ].pid      = 1;
+      pcb[ 0 ].status   = STATUS_CREATED;
+      pcb[ 0 ].ctx.cpsr = 0x50;                    // processor is switched into USR mode
+      pcb[ 0 ].ctx.pc   = ( uint32_t )( &main_P3 );
+      pcb[ 0 ].ctx.sp   = ( uint32_t )( &tos_P3  );
+
+      memset( &pcb[ 1 ], 0, sizeof( pcb_t ) );     // initialise 1-st PCB = P_4
+      pcb[ 1 ].pid      = 2;
+      pcb[ 1 ].status   = STATUS_CREATED;
+      pcb[ 1 ].ctx.cpsr = 0x50;
+      pcb[ 1 ].ctx.pc   = ( uint32_t )( &main_P4 );
+      pcb[ 1 ].ctx.sp   = ( uint32_t )( &tos_P4  );
+    
+      // Execute the 0th PCB chosen at random
+      dispatch( ctx, NULL, &pcb[ 0 ] );
+    
+      // Configuring the timer interrupt (from lab-4_q)
+      TIMER0->Timer1Load  = 0x00100000; // select period = 2^20 ticks ~= 1 sec
+      TIMER0->Timer1Ctrl  = 0x00000002; // select 32-bit   timer
+      TIMER0->Timer1Ctrl |= 0x00000040; // select periodic timer
+      TIMER0->Timer1Ctrl |= 0x00000020; // enable          timer interrupt
+      TIMER0->Timer1Ctrl |= 0x00000080; // enable          timer
+
+      GICC0->PMR          = 0x000000F0; // unmask all            interrupts
+      GICD0->ISENABLER1  |= 0x00000010; // enable timer          interrupt
+      GICC0->CTLR         = 0x00000001; // enable GIC interface
+      GICD0->CTLR         = 0x00000001; // enable GIC distributor
+      
+      // Enabling the IRQ interrupt
+      int_enable_irq();
+      
+      return;
 }
+
+// From lab-4_q
+void hilevel_handler_irq( ctx_t* ctx ) {
+    // Step 2: read  the interrupt identifier so we know the source.
+
+    uint32_t id = GICC0->IAR;
+
+    // Step 4: handle the interrupt, then clear (or reset) the source.
+
+    if( id == GIC_SOURCE_TIMER0 ) {
+      PL011_putc( UART0, 'T', true ); 
+      TIMER0->Timer1IntClr = 0x01;
+/**/  schedule( ctx );   // Switch process P_3 to P_4
+    }
+
+    // Step 5: write the interrupt identifier to signal we're done.
+
+    GICC0->EOIR = id;
+    return;
+}
+
+// From lab-3_q
+void hilevel_handler_svc( ctx_t* ctx, uint32_t id ) {
+   /* Based on the identifier (i.e., the immediate operand) extracted from the
+   * svc instruction,
+   *
+   * - read  the arguments from preserved usr mode registers,
+   * - perform whatever is appropriate for this system call, then
+   * - write any return value back to preserved usr mode registers.
+   */
+    switch( id ) {
+        case 0x00 : { // 0x00 => yield()
+          schedule( ctx );
+
+          break;
+        }
+
+        case 0x01 : { // 0x01 => write( fd, x, n )
+          int   fd = ( int   )( ctx->gpr[ 0 ] );
+          char*  x = ( char* )( ctx->gpr[ 1 ] );
+          int    n = ( int   )( ctx->gpr[ 2 ] );
+
+          for( int i = 0; i < n; i++ ) {
+            PL011_putc( UART0, *x++, true );
+          }
+
+          ctx->gpr[ 0 ] = n;
+
+          break;
+        }
+
+        default   : { // 0x?? => unknown/unsupported
+          break;
+        }
+    }
+    return;
+}
+
