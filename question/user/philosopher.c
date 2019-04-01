640c8db3fdc8af0b85886f9420f49540d6872a9b
Karthik Sridhar <ks17226@it052562.wks.bris.ac.uk>
2019-04-01 19:12:30 +0100

    still doesnt work, prints THINKING infinite

--- question/user/philosopher.c
+++ question/user/philosopher.c
@@ -5,41 +5,41 @@
 extern int find_philo_pid();
 // action => defining the action the philosopher must perform
 
-// Philosopher who wants to use the fork, so asks the person next 
+// Philosopher who wants to use the fork, so asks the person next
 void main_philosopher() {
-    int pid, pipeID;
-    
-    pid = find_philo_pid();
-    
+    int pipeID;
+
+    int pid = find_philo_pid();
+
     if ( pid == PHILS + 2 ) {
-        pipeID = pipe( 3 );       // choosing the end of the first pipe if we 
+        pipeID = pipe( 3 );       // choosing the end of the first pipe if we
                                   // receive the pid of the last pipe
     }
     else pipeID = pipe( pid + 1 );
-    
+
     int action = 2;
-    
-    if ( pid == 9 || pid == 3 ) {
+
+    if ( pid == 3 || pid == 9 ) {
         action = 1;
     }
-    
+
     while ( 1 ) {
-        if ( action == 1 ) { // if philosopher is eating now, then make him think 
+        if ( action == 1 ) { // if philosopher is eating now, then make him think
             write( STDOUT_FILENO, " THINKING ", 10 );
-            
+
             writePipe( pipeID, 1 );
             action = 2;     // making him think
-            
+
             if ( pipeID == PHILS - 1) // checking last process
-               write( STDOUT_FILENO, "\n", 2 ); 
+               write( STDOUT_FILENO, "\n", 2 );
             yield();
         }
         else {
-            if ( pid == 3 ) 
+            if ( pid == 3 )
                 action = readPipe( PHILS + 2 );
             else
                 action = readPipe( pid - 1 );
-            
+
             if ( action == 1) { // if philo can eat, make him eat
                 write( STDOUT_FILENO, " EATING ", 8 );
                 writePipe( pipeID, 2 );
@@ -54,4 +54,4 @@ void main_philosopher() {
         }
     }
     exit( EXIT_SUCCESS );
-} 
+}
--- question/user/waiter.c
+++ question/user/waiter.c
@@ -1,12 +1,12 @@
-/* ARBITRATOR SOLUTION 
+/* ARBITRATOR SOLUTION
  * In order to pick up the forks,
- * a philosopher must ask permission of the waiter. 
- * The waiter gives permission to only one philosopher at a time 
- * until the philosopher has picked up both of their forks. 
+ * a philosopher must ask permission of the waiter.
+ * The waiter gives permission to only one philosopher at a time
+ * until the philosopher has picked up both of their forks.
  * Putting down a fork is always allowed.
- * 
+ *
  * IMPLEMENTING THE WAITER AS A MUTEX
- * 
+ *
  * */
 
 #include "waiter.h"
@@ -30,7 +30,7 @@ void main_waiter() {
     // init the pipes array and philosopher PIDs array
     int pipes[ PIPES ];
     int pids[ PHILS ];
-    
+
     // Calling fork for each philosopher
     for ( int i = 0; i < PHILS; i++ ) {
         pids[ i ] = fork();   // forking a child process for each philosopher
@@ -41,6 +41,3 @@ void main_waiter() {
     }
     exit( EXIT_SUCCESS );
 }
-
-    
-
